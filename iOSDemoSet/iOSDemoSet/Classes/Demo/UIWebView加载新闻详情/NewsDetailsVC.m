b9e537599be3906644e5d2f95993ee5491284009
Jone <1427778161@qq.com>
2017-03-09 17:27:42 +0800

    新闻详情

--- iOSDemoSet/iOSDemoSet/Classes/Demo/UIWebView加载新闻详情/NewsDetailsVC.m
+++ iOSDemoSet/iOSDemoSet/Classes/Demo/UIWebView加载新闻详情/NewsDetailsVC.m
@@ -23,19 +23,27 @@
     
     self.title = @"";
     
+    
     [self.view addSubview:self.webView];
     
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://news.xinhuanet.com/photo/2017-02/27/c_129497220.htm"]];
-    [self.webView loadRequest:request];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://news.xinhuanet.com/photo/2017-02/27/c_129497220.htm"]];
+//    [self.webView loadRequest:request];
+//    
+//    UILabel *label = [UILabel new];
+//    label.backgroundColor = [UIColor whiteColor];
+//    label.text = @"我是label我是label我是label";
+//    [self.webView.scrollView addSubview:label];
+//    [label mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.left.right.equalTo(self.webView);
+//        make.bottom.equalTo(self.webView.scrollView.mas_bottom);
+//    }];
     
-    UILabel *label = [UILabel new];
-    label.backgroundColor = [UIColor whiteColor];
-    label.text = @"我是label我是label我是label";
-    [self.webView.scrollView addSubview:label];
-    [label mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.right.equalTo(self.webView);
-        make.bottom.equalTo(self.webView.scrollView.mas_bottom);
-    }];
+    NSError *error = nil;
+    
+    NSURL *url = [[NSBundle mainBundle] URLForResource:@"newDetail" withExtension:@"html"];
+    NSString *content = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:&error];
+    [self.webView loadHTMLString:content baseURL:[NSURL URLWithString:@"http://www.baidu.com"]];
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated {}
@@ -50,10 +58,22 @@
 
 #pragma mark - UIWebViewDelegate
 
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    [SVProgressHUD showInfoWithStatus:@"加载中..."];
+}
+
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
-    
+    [SVProgressHUD dismiss];
     DLog(@"webView.scrollView.contentSize = %@",NSStringFromCGSize(webView.scrollView.contentSize));
     
+}
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    if ([request.URL.absoluteString isEqualToString:@"http://120.25.95.75:8080/imgfile/uploadFiles/uploadImgs/html/img/20170309/1489024668126058214.jpg"]) {
+        return NO;
+    }
+    
+    return YES;
 }
 
 #pragma mark - UIScrollViewDelegate
